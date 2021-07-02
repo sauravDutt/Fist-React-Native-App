778bb9785831394ebed0d1afb4ee79ca65940e9d
sauravDutt <sauravd069@gmail.com>
2021-07-02 16:26:23 +0530

    Just added some more stuff

--- App.js
+++ App.js
@@ -1,58 +1,8 @@
 import * as React from "react";
-import {View, ActivityIndicator, Text, Button , ScrollView, TextInput, Image, Alert } from "react-native";
+import {View, Text, Button, KeyboardAvoidingView, ScrollView, StatusBar, TextInput, StyleSheet, Keyboard, TouchableWithoutFeedback, Platform, Image, Alert } from "react-native";
+import {useState} from 'react';
+import Login from "./components/Login";
 
-const Header = () => {
-  return (
-    <View 
-        style={{
-          overflow: "hidden",
-          top: 0,
-          position: 'fixed',
-        }}
-      >
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            marginTop: 40,
-          }}
-        >
-          <Button 
-            title='Add'
-            onPress={() => Alert.alert("Soon this button will let you add an insight, when I'll figure out how to add backend to a react native app")}
-          />
-          <Text
-            style={{
-              color: 'white',
-              marginTop: 4,
-              fontSize: 20,
-            }}
-          > 
-            insights
-          </Text>
-          <Button 
-            title='Info'
-            onPress={() => Alert.alert('This is just a practice, react native application, trying to cover all the important concepts of this framework and trying to master the art.')}
-          />
-        </View>
-          <TextInput
-            style={{
-              height: 40,
-              borderRadius: 10,
-              borderWidth: 0.1,
-              padding: 10,
-              marginTop: 20,
-              color: "white",
-              width: '100%',
-              borderColor: 'white',
-              backgroundColor: 'rgba(255, 255, 255, 0.031)'
-              
-            }}
-            placeholder="Search "
-          />  
-      </View>
-  );
-}
 const Footer = () => {
   return (
     <Text 
@@ -238,9 +188,9 @@ const Posts = () => {
   return (
     <ScrollView
         style={{
-          width:320,
+          width:330,
           height: 20,
-          padding: 10,
+          padding: 15,
           marginTop: 20,
           marginBottom: 15,
           borderRadius: 10,
@@ -270,22 +220,150 @@ const Caption = () => {
   )
 }
 const MainArea = () => {
-  return (
-    <View
+  const [login, setlogin] = useState(false);
+  const [signin, setSignin] = useState(false);
+  if (!login) {
+    return(
+      <View
       style={{
-        flex: 1,
-        padding: 26,
+        padding: 25,
         backgroundColor: "#1f1f1f",
       }}
     >
-      <Header />
-      <Caption />
-      <Posts />
-      <Footer />
+      < View 
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <View 
+        style={{
+          overflow: "hidden",
+          top: 0,
+          position: 'fixed',
+        }}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginTop: 40,
+          }}
+        >
+          <Button 
+            title='Add'
+            onPress={() => setlogin(true)}
+          />
+          <Text
+            style={{
+              color: 'white',
+              marginTop: 4,
+              fontSize: 20,
+            }}
+          > 
+            insights
+          </Text>
+          <Button 
+            title='Info'
+            onPress={() => Alert.alert('This is just a practice, react native application, trying to cover all the important concepts of this framework and trying to master the art, Nothing serious just practicing.')}
+          />
+        </View>
+          <TextInput
+            style={{
+              height: 40,
+              borderRadius: 10,
+              borderWidth: 0.1,
+              padding: 10,
+              marginTop: 20,
+              color: "white",
+              width: '100%',
+              borderColor: 'white',
+              backgroundColor: 'rgba(255, 255, 255, 0.031)'
+              
+            }}
+            placeholder="Search "
+          />  
+      </View>
+        <Caption />
+        <Posts />
+        <Footer />
+      </View>
 
     </View>
+    );
+  }
+  if(signin) {
+    return(
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <StatusBar
+        barStyle= 'dark-content'
+        />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Text style={styles.header}>Sign in</Text>
+          <TextInput placeholder="Any unique Username" style={styles.textInput} />
+          <TextInput placeholder ="Email id" style={styles.textInput}/>
+          <TextInput secureTextEntry={true} style={styles.textInput} placeholder="Password" />
+          <TextInput secureTextEntry={true} style={styles.textInput} placeholder="Confirm Password" />
+          <View style={styles.btnContainer}>
+            <Button title="Submit" onPress={() => setSignin(false)} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+    )
+  }
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <StatusBar
+        barStyle= 'dark-content'
+        />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Text style={styles.header}>Login</Text>
+          <TextInput placeholder="Username" style={styles.textInput} />
+          <TextInput secureTextEntry={true} style={styles.textInput} placeholder="Password" />
+          <View style={styles.btnContainer}>
+            <Button title="Login" onPress={() => setlogin(false)} />
+            <Button title="Sign in" onPress={() => setSignin(true)} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
-}
+  
+};
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  inner: {
+    padding: 40,
+    flex: 1,
+    justifyContent: "space-around"
+  },
+  header: {
+    fontSize: 36,
+    marginBottom: 48
+  },
+  textInput: {
+    height: 40,
+    borderColor: "#000000",
+    borderBottomWidth: 1,
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: "white",
+    marginTop: 12
+  }
+});
 export default MainArea;
  
